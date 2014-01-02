8242b0b281d84b981274c3f0b011e4d09403f32b
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2014-01-02 11:36:18 +0100

    Add some various scripts [again]

---
+++ bin/debugSonarSonar.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mvnDebug org.codehaus.sonar:sonar-maven3-plugin:2.4:sonar $*
---
+++ bin/mvn-switch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm $SOFTWARE_FOLDER/Maven/current
+
+if [ "$1" = "2" ] 
+then
+  ln -s $SOFTWARE_FOLDER/Maven/apache-maven-2.2.1/ $SOFTWARE_FOLDER/Maven/current
+else
+  if [ "$1" = "3.1" ] 
+  then
+    ln -s $SOFTWARE_FOLDER/Maven/apache-maven-3.1.0-alpha-1/ $SOFTWARE_FOLDER/Maven/current
+  else
+    ln -s $SOFTWARE_FOLDER/Maven/apache-maven-3.0.5/ $SOFTWARE_FOLDER/Maven/current
+  fi
+fi
+
+mvn -v
