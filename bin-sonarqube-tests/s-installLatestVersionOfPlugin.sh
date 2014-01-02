6b19235ad224cb14c28f826258070e49039b1a40
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2014-01-02 16:00:02 +0100

    Update plugin install script

--- bin-sonarqube-tests/s-installLatestVersionOfPlugin.sh
+++ bin-sonarqube-tests/s-installLatestVersionOfPlugin.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 PLUGIN=$1
+DEPLOY_FOLDER=$PLUGINS_DEV
+
+echo "Building Plugin $1"
+
+if [ "$2" = "-l" ]
+then 
+  EXTRA_PATH="sonar-$PLUGIN-plugin/"
+  echo "=> LANGUAGE PLUGIN"
+fi
 
 cd $REPOS/sonar-$PLUGIN
 git pull
 mvn clean install -DskipTests
 
-rm -f $PLUGINS_DEV/sonar-$PLUGIN-plugin-*.jar
-cp $REPOS/sonar-$PLUGIN/target/sonar-$PLUGIN-plugin-*-SNAPSHOT.jar $PLUGINS_DEV
+BINARY="$REPOS/sonar-$PLUGIN/$EXTRA_PATH/target/sonar-$PLUGIN-plugin-*-SNAPSHOT.jar"
+rm -f $DEPLOY_FOLDER/sonar-$PLUGIN-plugin-*.jar
+cp $BINARY $DEPLOY_FOLDER
