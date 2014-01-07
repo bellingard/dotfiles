3fdfe4fe9222e50267fef8ec299ac614dfd09189
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2014-01-07 12:20:22 +0100

    Add variable that allows to set the DB IP address

--- bin-sonarqube-prod/sonar.sh
+++ bin-sonarqube-prod/sonar.sh
@@ -2,22 +2,29 @@
 
 SONAR_PROPERTIES_FILE=$SOFTWARE_FOLDER/Sonar/current/conf/sonar.properties
 
+if [ -z "$3" ]
+then 
+  SONAR_DB="localhost"
+else
+  SONAR_DB="$3"
+fi
+
 if [ "$1" = "start" ]
 then 
   # print out properties for the correct DB
   if [ "$2" = "P" ] 
   then
-    echo "sonar.jdbc.url=jdbc:postgresql://localhost:15432/sonar" >> $SONAR_PROPERTIES_FILE
+    echo "sonar.jdbc.url=jdbc:postgresql://$SONAR_DB:15432/sonar" >> $SONAR_PROPERTIES_FILE
   else
     if [ "$2" = "M" ] 
     then
-      echo "sonar.jdbc.url=jdbc:mysql://localhost:13306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" >> $SONAR_PROPERTIES_FILE
+      echo "sonar.jdbc.url=jdbc:mysql://$SONAR_DB:13306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true" >> $SONAR_PROPERTIES_FILE
     else
       if [ "$2" = "O" ] 
       then
-        echo "sonar.jdbc.url=jdbc:oracle:thin:@localhost/XE" >> $SONAR_PROPERTIES_FILE
+        echo "sonar.jdbc.url=jdbc:oracle:thin:@$SONAR_DB/XE" >> $SONAR_PROPERTIES_FILE
       else
-        echo "sonar.jdbc.url=jdbc:h2:tcp://localhost:9092/sonar" >> $SONAR_PROPERTIES_FILE
+        echo "sonar.jdbc.url=jdbc:h2:tcp://$SONAR_DB:9092/sonar" >> $SONAR_PROPERTIES_FILE
       fi
     fi
   fi
