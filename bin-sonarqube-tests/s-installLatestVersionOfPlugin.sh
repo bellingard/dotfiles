fa8f41531de46429962a60f157f29cd26e9bff18
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2014-02-10 15:17:02 +0100

    Improve install of plugins

--- bin-sonarqube-tests/s-installLatestVersionOfPlugin.sh
+++ bin-sonarqube-tests/s-installLatestVersionOfPlugin.sh
@@ -2,18 +2,39 @@
 PLUGIN=$1
 DEPLOY_FOLDER=$PLUGINS_DEV
 
-echo "Building Plugin $1"
-
-if [ "$2" = "-l" ]
-then 
-  EXTRA_PATH="sonar-$PLUGIN-plugin/"
-  echo "=> LANGUAGE PLUGIN"
-fi
+echo "Building SonarQube $1"
 
 cd $REPOS/sonar-$PLUGIN
 git pull
 mvn clean install -DskipTests
 
-BINARY="$REPOS/sonar-$PLUGIN/$EXTRA_PATH/target/sonar-$PLUGIN-plugin-*-SNAPSHOT.jar"
-rm -f $DEPLOY_FOLDER/sonar-$PLUGIN-plugin-*.jar
-cp $BINARY $DEPLOY_FOLDER
+installPluginInDeployFolder() {
+  if [ "$2" = "" ]
+  then 
+    # Simple plugin
+    NAME=$1
+    BINARY="$REPOS/sonar-$PLUGIN/target/sonar-$PLUGIN-plugin-*-SNAPSHOT.jar"
+  else
+  	# Multi-JAR plugin
+    NAME=$2
+  	BINARY="$REPOS/sonar-$PLUGIN/sonar-$2-plugin/target/sonar-$2-plugin-*-SNAPSHOT.jar"
+  fi
+  
+  echo "... Installing $NAME"
+  rm -f $DEPLOY_FOLDER/sonar-$NAME-plugin-*.jar
+  cp $BINARY $DEPLOY_FOLDER
+}
+
+PLUGINS_LIST=`ls $REPOS/sonar-$PLUGIN | grep plugin | sed 's/sonar-\(.*\)-plugin/\1/' `
+
+if [ "$PLUGINS_LIST" = "" ]
+then
+  # Simple plugin
+  installPluginInDeployFolder $PLUGIN
+else
+  # Multi-JAR plugin
+  for p in $PLUGINS_LIST; do
+    installPluginInDeployFolder $PLUGIN $p
+  done
+fi
+
