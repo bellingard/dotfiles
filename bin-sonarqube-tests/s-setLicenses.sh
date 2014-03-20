abc8dd52ffa91e3d409de2093570d402f209dbb8
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2014-03-20 15:15:05 +0100

    Double curl because all the plugins do not have the same property...

--- bin-sonarqube-tests/s-setLicenses.sh
+++ bin-sonarqube-tests/s-setLicenses.sh
@@ -4,7 +4,9 @@
 
 setLicense() {
   LICENSE=`curl -s $SONARSOURCE_PLUGIN_LICENSES_URL/$1.txt | tr -d '\r\n'`
+  # double curl because all the plugins do not have the same property...
   curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=$1.license.secured&value=$LICENSE"
+  curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=sonar.$1.license.secured&value=$LICENSE"
 }
 
 setLicense "views"
