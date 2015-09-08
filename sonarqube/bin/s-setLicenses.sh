74d7b253e3a17a60e03e86638bded2f180a2e60d
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2015-09-08 14:53:35 +0200

    Update script to retrieve licenses

--- sonarqube/bin/s-setLicenses.sh
+++ sonarqube/bin/s-setLicenses.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# Need to have $SONARSOURCE_PLUGIN_LICENSES_URL defined as environment variable
-
 setLicense() {
-  LICENSE=`curl -s $SONARSOURCE_PLUGIN_LICENSES_URL/$1.txt | tr -d '\r\n'`
+  LICENSE=`more $REPOS/licenses/it/$1.txt | tr -d '\r\n'`
   # double curl because all the plugins do not have the same property...
   curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=$1.license.secured&value=$LICENSE"
   curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=sonar.$1.license.secured&value=$LICENSE"
@@ -16,3 +14,14 @@ setLicense "report"
 
 # some language plugins too
 setLicense "cpp"
+
+# -----------------------------------------------------------------------------
+# OLD WAY TO GET LICENSES
+# Need to have $SONARSOURCE_PLUGIN_LICENSES_URL defined as environment variable
+setLicenseOLD() {
+  LICENSE=`curl -s $SONARSOURCE_PLUGIN_LICENSES_URL/$1.txt | tr -d '\r\n'`
+  # double curl because all the plugins do not have the same property...
+  curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=$1.license.secured&value=$LICENSE"
+  curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=sonar.$1.license.secured&value=$LICENSE"
+}
+# -----------------------------------------------------------------------------
