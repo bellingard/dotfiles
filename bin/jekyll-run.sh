e0bcb5e557d056a7130d5314eaa2c56eb1b6d14d
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2016-12-06 15:07:44 +0100

    Add 2nd Jekyll script

--- bin/jekyll-run.sh
+++ bin/jekyll-run.sh
@@ -2,4 +2,4 @@
 
 # Runs Jekyll from its docker image
 
-docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 jekyll/jekyll
+docker run --rm --label=jekyll --volume=$(pwd):/srv/jekyll -it -p 127.0.0.1:4000:4000 jekyll/jekyll bundle exe jekyll serve --incremental
