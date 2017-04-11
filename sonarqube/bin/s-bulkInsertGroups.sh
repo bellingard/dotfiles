60ff528c51155cf7976f6c56cb0da35a12d4db49
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2017-04-11 18:03:33 +0200

    Fix wrong URL

--- sonarqube/bin/s-bulkInsertGroups.sh
+++ sonarqube/bin/s-bulkInsertGroups.sh
@@ -1,10 +1,10 @@
 x=1
 while [ $x -le 100 ]
 do
-  
+
   echo "Group #$x"
-  random_name=$(curl http://www.setgetgo.com/randomword/get.php | tr -d '[[:space:]]') 
-  curl -u admin:admin -X POST "http://localhost:9000/api/usergroups/create?name=$random_name&description=Group-$x"
+  random_name=$(curl http://www.setgetgo.com/randomword/get.php | tr -d '[[:space:]]')
+  curl -u admin:admin -X POST "http://localhost:9000/api/user_groups/create?name=$random_name&description=Group-$x"
   echo " "
 
   x=$(( $x + 1 ))
