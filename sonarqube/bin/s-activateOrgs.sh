1d1963fb6a7d80ff64d1e7ff836f020b0a91bad5
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2017-04-20 11:23:09 +0200

    Rename some files

--- sonarqube/bin/s-activateOrgs.sh
+++ sonarqube/bin/s-activateOrgs.sh
@@ -5,9 +5,11 @@ set -e
 
 # Activate orgs
 curl -X POST -u admin:admin http://localhost:9000/api/organizations/enable_support
-curl --data "key=sonar.lf.sonarqube.com.enabled&value=true" -X POST -u admin:admin http://localhost:9000/api/settings/set
 curl --data "key=sonar.organizations.anyoneCanCreate&value=true" -X POST -u admin:admin http://localhost:9000/api/settings/set
 curl --data "key=sonar.organizations.createPersonalOrg&value=true" -X POST -u admin:admin http://localhost:9000/api/settings/set
+
+# Not related to orgs but to SQ.com l&f...
+curl --data "key=sonar.lf.sonarqube.com.enabled&value=true" -X POST -u admin:admin http://localhost:9000/api/settings/set
 
 # Add some users
 curl --data "login=fab&name=Fabrice&password=admin&password_confirmation=admin" -X POST -u admin:admin "http://localhost:9000/api/users/create"
