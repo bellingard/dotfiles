ff743240ac96c9d071ae604806532d296260c91d
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2017-06-29 10:21:40 +0200

    Allow to install any stable build

--- sonarqube/bin/s-installBuild.sh
+++ sonarqube/bin/s-installBuild.sh
@@ -3,17 +3,30 @@
 # Exit on failure
 set -e
 
-echo "Install latest stable build of SonarQube"
-
-LATEST=$(curl --silent "https://$BURGRX_USER:$BURGRX_PASS@burgrx.sonarsource.com/api/commitPipelinesStages?project=SonarSource/sonarqube&branch=master&nbOfCommits=50" | jq '[.[].pipelines[] | select(.stages[].type == "promotion")] | .[0]')
-if [ "$LATEST" = "null" ]
+if [[ "$1" = "" ]]
 then
-		echo "No latest stable build."
-		exit 1
-fi
+	echo "Install latest stable build of SonarQube"
+	LATEST=$(curl --silent "https://$BURGRX_USER:$BURGRX_PASS@burgrx.sonarsource.com/api/commitPipelinesStages?project=SonarSource/sonarqube&branch=master&nbOfCommits=50" | jq '[.[].pipelines[] | select(.stages[].type == "promotion")] | .[0]')
+	if [ "$LATEST" = "null" ]
+	then
+			echo "No latest stable build."
+			exit 1
+	fi
 
-VERSION=$(echo $LATEST | jq -r '.version')
-URL=$(echo $LATEST | jq -r '.versionUrl')
+	VERSION=$(echo $LATEST | jq -r '.version')
+	URL=$(echo $LATEST | jq -r '.versionUrl')
+else
+	echo "Install build of SonarQube $1"
+	BUILD=$(curl --silent "https://$BURGRX_USER:$BURGRX_PASS@burgrx.sonarsource.com/api/commitPipelinesStages?project=SonarSource/sonarqube&branch=master&nbOfCommits=50" | jq --arg VERSION "$1" '[.[].pipelines[] | select(.stages[].type == "promotion") | select(.version == $VERSION)] | .[0]')
+	if [ "$BUILD" = "null" ]
+	then
+			echo "No stable build for version $1."
+			exit 1
+	fi
+
+	VERSION=$1
+	URL=$(echo $BUILD | jq -r '.versionUrl')
+fi
 
 echo "Downloading SonarQube $VERSION"
 curl -L -# -o "$SOFTWARE_FOLDER/SonarQube/_TEMP_/sonarqube-$VERSION.zip" "$URL"
