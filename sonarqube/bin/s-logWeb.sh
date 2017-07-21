f953950dab1db6f7692b958c257d632f6d651e17
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2017-07-21 11:45:15 +0200

    Wait a bit in case the Web log file does not exist

--- sonarqube/bin/s-logWeb.sh
+++ sonarqube/bin/s-logWeb.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-tail -f $SONAR_CURRENT/logs/web.log
+LOG_FILE=$SONAR_CURRENT/logs/web.log
+count=0
+while [ ! -f $LOG_FILE ]
+do
+  echo "Log file not found, waiting..."
+  sleep 3
+  ((count++))
+  if [ $count -gt 4 ]; then
+    exit 0
+  fi
+done
+tail -f $LOG_FILE
