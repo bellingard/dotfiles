8afad9621f5389d3f97a4a7f58ecd8d7340dc860
Fabrice Bellingard <fabrice.bellingard@sonarsource.com>
2017-10-19 16:28:30 +0200

    Update some scripts

--- sonarqube/bin/s-runDevCockpit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# This is a temporary script that will become useless in 5.3+ hopefully
-
-sonar-runner devcockpit -Dsonar.login=admin -Dsonar.password=admin $*
--- sonarqube/bin/s-runViews.sh
+++ sonarqube/bin/s-runViews.sh
@@ -2,4 +2,4 @@
 
 # This is a temporary script that will become useless in 5.3+ hopefully
 
-sonar-runner views -Dsonar.login=admin -Dsonar.password=admin $*
+sonar-scanner views -Dsonar.login=admin -Dsonar.password=admin $*
