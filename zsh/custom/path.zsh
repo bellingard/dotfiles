6e5713a74fd5be3dd9314529b3ea4ded2c8ee908
Fabrice Bellingard <bellingard@gmail.com>
2025-11-05 13:48:01 +0100

    Add Whisper.cpp path on Ubuntu

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -9,4 +9,4 @@ export REPOS=$HOME/Repos
 # JAVA_HOME folder
 export JAVA_HOME=/usr/local/Cellar/openjdk/19.0.1/
 
-export PATH="$HOME/Applications/whisper.cpp/build/bin:$PATH"
+export PATH="$HOME/Applications/whisper.cpp/build/bin:$HOME/AppEvaluation/server-tools/whisper.cpp/build/bin:$PATH"
