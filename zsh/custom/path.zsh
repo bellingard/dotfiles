19505abec622730cb8f047b45a045fcdecf77556
Fabrice Bellingard <bellingard@gmail.com>
2026-01-21 21:27:24 +0100

    Add some PATHs

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -9,4 +9,12 @@ export REPOS=$HOME/Repos
 # JAVA_HOME folder
 export JAVA_HOME=/usr/local/Cellar/openjdk/19.0.1/
 
-export PATH="$HOME/Applications/whisper.cpp/build/bin:$HOME/AppEvaluation/server-tools/whisper.cpp/build/bin:$PATH"
+# Whisper.cpp for local transcription
+export PATH="$HOME/Applications/whisper.cpp/build/bin:$PATH"
+
+# Antigravity IDE by Google
+export PATH="$HOME/.antigravity/antigravity/bin:$PATH"
+
+# Local Bin folder
+export PATH="$HOME/.local/bin:$PATH"
+
