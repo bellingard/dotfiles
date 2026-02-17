eb307dce076daab19fe6a595d11208d002c04796
Fabrice Bellingard <bellingard@gmail.com>
2026-02-17 15:23:25 +0100

    Update Path

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -7,7 +7,8 @@ export REPOS=$HOME/Repos
 # Tools location and environment variables
 # ================================
 # JAVA_HOME folder
-export JAVA_HOME=/usr/local/Cellar/openjdk/19.0.1/
+export JAVA_HOME=/opt/homebrew/opt/openjdk
+export PATH="$JAVA_HOME/bin:$PATH"
 
 # Whisper.cpp for local transcription
 export PATH="$HOME/Applications/whisper.cpp/build/bin:$PATH"
